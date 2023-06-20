19f4ca420af787f967c9ad4bff964933e8ddd495
programadorLhama <programadorLhama@gmail.com>
2023-06-20 01:27:45 -0300

    feat: Implementing select and tests in UsersRepository

--- src/infra/db/repositories/users_repository.py
+++ src/infra/db/repositories/users_repository.py
@@ -18,3 +18,18 @@ class UsersRepository:
             except Exception as exception:
                 database.session.rollback()
                 raise exception
+
+    @classmethod
+    def select_user(cls, first_name: str) -> any:
+        with DBConnectionHandler() as database:
+            try:
+                users = (
+                    database.session
+                        .query(UsersEntity)
+                        .filter(UsersEntity.first_name == first_name)
+                        .all()
+                )
+                return users
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
--- src/infra/db/repositories/users_repository_test.py
+++ src/infra/db/repositories/users_repository_test.py
@@ -1,6 +1,12 @@
+import pytest
+from sqlalchemy import text
+from src.infra.db.settings.connection import DBConnectionHandler
 from .users_repository import UsersRepository
 
+db_connection_handler = DBConnectionHandler()
+connection = db_connection_handler.get_engine().connect()
 
+@pytest.mark.skip(reason="Sensive test")
 def test_insert_user():
     mocked_first_name = 'first'
     mocked_last_name = 'last'
@@ -8,3 +14,45 @@ def test_insert_user():
 
     users_repository = UsersRepository()
     users_repository.insert_user(mocked_first_name, mocked_last_name, mocked_age)
+
+    sql = '''
+        SELECT * FROM users
+        WHERE first_name = '{}'
+        AND last_name = '{}'
+        AND age = {}
+    '''.format(mocked_first_name, mocked_last_name, mocked_age)
+    response = connection.execute(text(sql))
+    registry = response.fetchall()[0]
+
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+
+    connection.execute(text(f'''
+        DELETE FROM users WHERE id = {registry.id}
+    '''))
+    connection.commit()
+
+@pytest.mark.skip(reason="Sensive test")
+def test_select_user():
+    mocked_first_name = 'first_2'
+    mocked_last_name = 'last_2'
+    mocked_age = 51
+
+    sql = '''
+        INSERT INTO users (first_name, last_name, age) VALUES ('{}', '{}', '{}')
+    '''.format(mocked_first_name, mocked_last_name, mocked_age)
+    connection.execute(text(sql))
+    connection.commit()
+
+    users_repository = UsersRepository()
+    response = users_repository.select_user(mocked_first_name)
+
+    assert response[0].first_name == mocked_first_name
+    assert response[0].last_name == mocked_last_name
+    assert response[0].age == mocked_age
+
+    connection.execute(text(f'''
+        DELETE FROM users WHERE id = {response[0].id}
+    '''))
+    connection.commit()
